53c92d6203200032cf43d5d6ddbbdb12eb4bc10d
Phat Nguyen <phat.nguyen@lockernlock.vn>
2025-09-14 07:14:13 +0700

    feat(persistence): add configurations for identity entities
    Introduce new configurations for assignable entities, permissions, role
    claims, user claims, and user roles in the Entity Framework Core context.
    The `AssignableEntityConfiguration` class is added to manage properties
    and indexes for `IAssignable` entities. New configuration classes for
    `Permission`, `RoleClaim`, `UserClaim`, and `UserRole` define their
    relationships and constraints. Removed outdated configurations for
    `IdentityRoleClaim`, `IdentityUserClaim`, and `IdentityUserRole` to
    streamline identity management.

---
+++ src/Infrastructure/Persistence/Configurations/Common/AssignableEntityConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using SharedKernel.Domain.Attributes;
+
+using UseCases.Common.Persistence.Constants;
+
+
+namespace Infrastructure.Persistence.Configurations.Common;
+public class AssignableEntityConfiguration<TEntity> : IEntityTypeConfiguration<TEntity>
+    where TEntity : class, IAssignable
+{
+    public void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+        builder.Property(propertyExpression: e => e.AssignedBy)
+            .HasMaxLength(maxLength: Constraints.CreatedByMaxLength);
+
+        builder.HasIndex(indexExpression: e => e.AssignedAt);
+        builder.HasIndex(indexExpression: e => e.AssignedBy);
+    }
+}
---
+++ src/Infrastructure/Persistence/Configurations/Identity/UserRoleConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Core.Identity;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations.Identity;
+
+public class UserRoleConfiguration : IEntityTypeConfiguration<UserRole>
+{
+    public void Configure(EntityTypeBuilder<UserRole> builder)
+    {
+        builder.HasOne(ur => ur.User)
+               .WithMany(u => u.UserRoles)
+               .HasForeignKey(ur => ur.UserId)
+               .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(ur => ur.Role)
+               .WithMany(r => r.UserRoles)
+               .HasForeignKey(ur => ur.RoleId)
+               .OnDelete(DeleteBehavior.Cascade);
+    }
+}
