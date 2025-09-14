5b7c928db661b03152f9f7f6ee6161109f748e81
Phat Nguyen <phat.nguyen@lockernlock.vn>
2025-09-14 07:12:12 +0700

    feat(identity): add UserClaim class and permission error handling
    Introduce a new `UserClaim` class extending `IdentityUserClaim<Guid>`
    to manage user assignment details, including properties for
    `AssignedAt`, `AssignedBy`, and a method `MarkAsAssigned`.
    
    Enhance `UserPermission.Errors` with a static class that provides
    predefined error messages for user permission management, covering
    scenarios like assignment failures, maximum permissions exceeded,
    and permission conflicts. This improves error handling and
    maintainability in permission-related operations.

---
+++ src/Core/Identity/UserClaim.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+using SharedKernel.Domain.Attributes;
+
+namespace Core.Identity;
+
+public sealed class UserClaim : IdentityUserClaim<Guid>, IAssignable
+{
+    public DateTimeOffset? AssignedAt { get; set; }
+    public string? AssignedBy { get; set; }
+
+    public User User { get; set; } = default!;
+
+    public void MarkAsAssigned(string? userId = null)
+    {
+        AssignedBy = userId;
+        AssignedAt = DateTimeOffset.UtcNow;
+    }
+}
